2290c77e143ee5d364c89fc2640d68f63fc4b892
Salchy <leandrocg524@gmail.com>
2026-08-14 14:53:17 -0300

    Avances en appConsola3

--- AppConsola3/src/dominio/Principal.java
+++ AppConsola3/src/dominio/Principal.java
@@ -12,6 +12,22 @@ public class Principal {
 		System.out.println(a.toString());
 		System.out.println(perro.toString());
 		System.out.println(gato.toString());
+		
+		// Polimorfismo
+		// Un objeto de la clase Hija, puede guardarse dentro de la clase Padre
+		
+		Animal xxx = new Perro();
+		
+		//////////
+		
+		Animal[] animales = new Animal[3];
+		animales[0] = a;
+		animales[1] = gato;
+		animales[2] = perro;
+		
+		for (Animal animal : animales) {
+			System.out.println(animal.toString());
+		}
 	}
 
 }
