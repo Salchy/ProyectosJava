e2f64798ade0133e2117efd8ae6d6bb125e1e48f
Salchy <leandrocg524@gmail.com>
2026-08-18 10:33:42 -0300

    Added: Metodo main

---
+++ TP1/src/EJERCICIO01/Principal.java
@@ -0,0 +1,15 @@
+package EJERCICIO01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Empleado emp1 = new Empleado();
+		Empleado emp2 = new Empleado();
+		Empleado emp3 = new Empleado();
+		
+		System.out.println(emp1.toString());
+		System.out.println(emp2.toString());
+		System.out.println(emp3.toString());
+	}
+
+}
